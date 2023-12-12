0df68eaedf7989c0ae911423fe9f06d3b2e7d343
MikeKinetex <mike@kinetex.io>
2023-12-12 16:21:48 +0400

    feat: get rid of threshold input

--- circuits/vars.rs
+++ circuits/vars.rs
@@ -2,23 +2,18 @@
 use crate::consts::HEADER_BYTES_LENGTH;
 
 use plonky2x::prelude::{
-    Bytes32Variable, BytesVariable, U256Variable, U32Variable, ArrayVariable,
+    Bytes32Variable, BytesVariable, U256Variable,
     CircuitVariable, Variable, CircuitBuilder, PlonkParameters, RichField
 };
 
 pub type HeaderBytesVariable = BytesVariable<HEADER_BYTES_LENGTH>;
 
 pub type BlockHashVariable = Bytes32Variable;
-pub type ThresholdVariable = Bytes32Variable;
 pub type WorkVariable = U256Variable;
 
 #[derive(Debug, Clone, CircuitVariable)]
 #[value_name(BitcoinHeaderType)]
 pub struct BitcoinHeaderVariable {
     pub hash: BlockHashVariable,
-    pub threshold: ThresholdVariable,
     pub work: WorkVariable,
-    pub raw: HeaderBytesVariable,
 }
-
-pub type ThresholdIntBytesVariable = ArrayVariable<U32Variable, 32>;
